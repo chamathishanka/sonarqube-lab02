aa8d891340261d03ab5f995e89ff2d47dfe65193
chamathishanka <pkchamath2000@gmail.com>
2026-02-01 20:30:10 +0530

    Fix quality issues

--- src/main/java/com/example/UserService.java
+++ src/main/java/com/example/UserService.java
@@ -2,6 +2,7 @@ package main.java.com.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class UserService {
@@ -10,18 +11,18 @@ public class UserService {
     private String password = "admin123";
 
     // VULNERABILITY: SQL Injection
-    public void findUser(String username) throws Exception {
+    public void findUser(String username) throws SQLException {
 
-        Connection conn =
+        try (Connection conn =
             DriverManager.getConnection("jdbc:mysql://localhost/db",
                     "root", password);
+             Statement st = conn.createStatement()) {
 
-        Statement st = conn.createStatement();
+            String query =
+                "SELECT * FROM users WHERE name = '" + username + "'";
 
-        String query =
-            "SELECT * FROM users WHERE name = '" + username + "'";
-
-        st.executeQuery(query);
+            st.executeQuery(query);
+        }
     }
 
     // SMELL: Unused method
@@ -30,14 +31,15 @@ public class UserService {
     }
 
     // EVEN WORSE: another SQL injection
-    public void deleteUser(String username) throws Exception {
-        Connection conn =
+    public void deleteUser(String username) throws SQLException {
+        try (Connection conn =
             DriverManager.getConnection("jdbc:mysql://localhost/db",
                     "root", password);
-        Statement st = conn.createStatement();
-        String query =
-            "DELETE FROM users WHERE name = '" + username + "'";
-        st.execute(query);
+             Statement st = conn.createStatement()) {
+            String query =
+                "DELETE FROM users WHERE name = '" + username + "'";
+            st.execute(query);
+        }
     }
 
 }
